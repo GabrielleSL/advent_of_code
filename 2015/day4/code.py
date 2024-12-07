77785d88d1a114df622722457dfbe440c5eb0162
GabrielleSL <sunshinegabrielle@gmail.com>
2024-12-06 23:55:15 -0500

    2015 day 4

--- 2015/day4/code.py
+++ 2015/day4/code.py
@@ -1,8 +1,21 @@
-def part1():
-    f = open("input.txt", "r").readline()
+import hashlib
 
-def part2():
-    f = open("input.txt", "r").readline()
+secret_key = 'yzbqklnj'
 
-part1()
-# part2()
+ans1 = 0
+ans2 = 0
+
+for i in range(10000000):
+    key_num = secret_key + str(i)
+
+    h = hashlib.md5(key_num.encode())
+    if ans1 == 0 and h.hexdigest()[:5] == '00000':
+        ans1 = i
+    if ans2 == 0 and h.hexdigest()[:6] == '000000':
+        ans2 = i
+
+    if ans1 != 0 and ans2 != 0:
+        break
+
+print("PART 1:", ans1)
+print("PART 2:", ans2)
